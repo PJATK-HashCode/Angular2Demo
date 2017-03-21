66a5d7fafd1c0c3970e5ed4a425790bb43913043
Adam Urbanowicz <adam.urbanow@gmail.com>
2017-03-21 17:01:39 +0100

    add new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TitleModel } from "./title-list/title-list.component";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  
+  public isLoading:boolean = true;
+  public titles:Array<TitleModel> = [];
+  
+  private _title : string;
+
+  public OnLoading(loading:boolean):void{
+    this.isLoading=loading;
+    //alert(loading);
+  }
+
+  public get title() : string {
+    return this._title;
+  }
+
+  public set title(v : string) {
+    this._title = v;
+  }
+  
+  public addTitle():void{
+    this.titles.push(new TitleModel(this._title,true));
+    this._title='';
+  }
+
+  public constructor(){
+
+    this.title = "Hello";
+  }
+
+  public ngOnInit():void{
+      
+
+
+  }
+
 }
