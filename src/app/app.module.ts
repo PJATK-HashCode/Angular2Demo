31cac7f87bccf30b89f5732bbbff74bf021288ff
Adam Urbanowicz <adam.urbanow@gmail.com>
2017-03-21 17:51:38 +0100

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,31 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import {AppComponent} from './app.component';
-import {TitleModule} from './title/title.module';
-import { ComponentComponent } from './component/component.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from "./login/login.component";
+
+import { RouterModule, Routes } from '@angular/router';
+import { TitleModule } from "./title-module/title.module";
+
+const routes: Routes = [
+  { path: "titles", component: AppComponent },
+  { path: "login", component: LoginComponent },
+  { path: "", redirectTo: "/app", pathMatch:'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentComponent
-    
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    TitleModule
+    TitleModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
